3a416e3fcb12aefa088b103de4a6b8c4edf36ef8
chenrenhao <1045617063@qq.com>
2018-03-24 16:33:36 +0800

    添加了一些公共工具类

---
+++ src/main/java/com/template/util/math/RandomUtil.java
@@ -0,0 +1,8 @@
+package com.template.util.math;
+
+/**
+ * Created by ChenRenhao on 2018/3/24.
+ */
+public class RandomUtil {
+
+}
---
+++ src/main/java/com/template/util/security/des/DesUtil.java
@@ -0,0 +1,135 @@
+package com.template.util.security.des;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+
+public class DesUtil {
+ 
+	//private static Logger logger = LoggerFactory.getLogger(DesUtil.class);
+	
+    private final static String DES = "DES";
+ 
+    /**
+     * Description 根据键值进行加密
+     * @param data 
+     * @param key  加密键byte数组
+     * @return
+     * @throws Exception
+     */
+    public static String encrypt(String data, String key) throws Exception {
+        byte[] bt = encrypt(data.getBytes("utf-8"), key.getBytes("utf-8"));
+        String strs =Base64.getEncoder().encodeToString(bt);
+        return strs;
+    }
+ 
+    /**
+     * Description 根据键值进行解密
+     * @param data
+     * @param key  加密键byte数组
+     * @return
+     * @throws IOException
+     * @throws Exception
+     */
+    public static String decrypt(String data, String key) throws IOException,
+            Exception {
+        if (data == null)
+            return null;
+        
+        byte[] buf = Base64.getDecoder().decode(data);
+        byte[] bt = decrypt(buf,key.getBytes("utf-8"));
+        return new String(bt, "utf-8");
+    }
+ 
+    /**
+     * Description 根据键值进行加密
+     * @param data
+     * @param key  加密键byte数组
+     * @return
+     * @throws Exception
+     */
+    private static byte[] encrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+ 
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+ 
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+        SecretKey securekey = keyFactory.generateSecret(dks);
+ 
+        // Cipher对象实际完成加密操作
+        Cipher cipher = Cipher.getInstance(DES);
+ 
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
+ 
+        return cipher.doFinal(data);
+    }
+     
+     
+    /**
+     * Description 根据键值进行解密
+     * @param data
+     * @param key  加密键byte数组
+     * @return
+     * @throws Exception
+     */
+    private static byte[] decrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+ 
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+ 
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+        SecretKey securekey = keyFactory.generateSecret(dks);
+ 
+        // Cipher对象实际完成解密操作
+        Cipher cipher = Cipher.getInstance(DES);
+ 
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
+ 
+        return cipher.doFinal(data);
+    }
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
